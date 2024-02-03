950dacfe513bb680edd6a7bb1c0e26c7019e8164
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2024-02-03 02:30:03 +0100

    Refactor remove unused files

--- src/components/common/button/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from "react";
-import { Button as ButtonMaterialUI } from "@mui/material";
-
-type ButtonMaterialUIProps = React.ComponentProps<typeof ButtonMaterialUI>;
-
-export const Button: FC<ButtonMaterialUIProps> = ({ ...props }) => {
-  return <ButtonMaterialUI {...props}>{props.children}</ButtonMaterialUI>;
-};
--- src/components/common/skeleton/Skeleton.tsx
+++
@@ -1,8 +0,0 @@
-import { Skeleton as SkeletonMaterialUI } from "@mui/material";
-import { FC } from "react";
-
-type SkeletonMaterialUIProps = React.ComponentProps<typeof SkeletonMaterialUI>;
-
-export const Skeleton: FC<SkeletonMaterialUIProps> = ({ ...props }) => {
-  return <Skeleton {...props} />;
-};
