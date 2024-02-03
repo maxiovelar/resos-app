7538d1d24b54a29058063e66e128140d9faaa8a1
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2024-02-03 03:12:06 +0100

    Add Loader component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,6 @@
 export * from "./layout/Layout.tsx";
 export * from "./navbar/Navbar.tsx";
 export * from "./header-shape/HeaderShape.tsx";
-export * from "./skeleton/Skeleton.tsx";
+export * from "./loader/Loader.tsx";
 export * from "./search-section/SearchSection.tsx";
 export * from "./state-container/StateContainer.tsx";
