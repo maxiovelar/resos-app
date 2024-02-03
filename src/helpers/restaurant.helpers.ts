5337591c2ad499c813789a6f2efe03c5bf586774
Maxi Ovelar <73977645+maxiovelar@users.noreply.github.com>
2024-02-03 16:49:24 +0100

    Refactor functions

--- src/helpers/restaurant.helpers.ts
+++ src/helpers/restaurant.helpers.ts
@@ -11,6 +11,15 @@ export const isRestaurantOpen = ({
   const currentHour = new Date().getHours();
   const currentMinute = new Date().getMinutes();
 
+  const validateHourFormat = (hour: string): boolean => {
+    const hourRegex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+    return hourRegex.test(hour);
+  };
+
+  if (!validateHourFormat(openHour) || !validateHourFormat(closeHour)) {
+    throw new Error("Invalid hour format");
+  }
+
   const [restaurantOpenHour, restaurantOpenMinute] = openHour
     .split(":")
     .map(Number);
@@ -22,11 +31,12 @@ export const isRestaurantOpen = ({
   const restaurantCloseTime = restaurantCloseHour * 60 + restaurantCloseMinute;
   const currentTime = currentHour * 60 + currentMinute;
 
-  return currentTime >= restaurantOpenTime && currentTime <= restaurantCloseTime
-    ? true
-    : false;
+  return (
+    currentTime >= restaurantOpenTime && currentTime <= restaurantCloseTime
+  );
 };
 
 export const generateImageUrl = (image: string) => {
-  return `${imagesUrl}/${image}`;
+  const url = new URL(image, imagesUrl);
+  return url.toString();
 };
